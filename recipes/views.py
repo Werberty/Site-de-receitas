6f885bcf30147ce45a2021170338f8c369ea0840
Werberty <werberty.alexandre@gmail.com>
2022-05-30 17:46:58 -0300

    Adicionando uma nova url para uma nova view

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Werberty'
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Werberty'
+    })
