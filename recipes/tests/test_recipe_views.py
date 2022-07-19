266a6d48c97f4d1c9ba9edf6358232d1fd76f79f
Werberty <werberty.alexandre@gmail.com>
2022-07-18 22:31:08 -0300

    FEAT: Criando fixtures para um teste unico(Recipes, Category e Author)

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,6 +1,8 @@
+
 from django.test import TestCase
 from django.urls import resolve, reverse
 from recipes import views
+from recipes.models import Category, Recipe, User
 
 
 class RecipeViewsTest(TestCase):
@@ -23,9 +25,35 @@ class RecipeViewsTest(TestCase):
             response.content.decode('utf-8')
         )
 
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@rmail.com',
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe title',
+            description='Recipe description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+        response = self.client.get(reverse('recipes:home'))
+        ...
+
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category',
-                       kwargs={'category_id': 1000}))
+                               kwargs={'category_id': 1000}))
         self.assertIs(view.func, views.category)
 
     def test_recipe_category_returns_404_if_no_recipe_found(self):
