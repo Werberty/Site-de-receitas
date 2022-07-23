44494cca2f4128557b68d1cf4888b96453f92286
Werberty <werberty.alexandre@gmail.com>
2022-07-23 08:52:28 -0300

    TEST: category e detail carregam a receita correta

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,8 +1,7 @@
-
+from unittest import skip
 
 from django.urls import resolve, reverse
 from recipes import views
-from recipes.models import Recipe
 
 from .test_recipe_base import RecipeTestBase
 
@@ -48,6 +47,17 @@ class RecipeViewsTest(RecipeTestBase):
             reverse('recipes:category', kwargs={'category_id': 1000}))
         self.assertEqual(response.status_code, 404)
 
+    def test_recipe_category_template_loads_recipes(self):
+        needed_title = 'This is a category test'
+        # need a recipe for this test
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        content = response.content.decode('utf-8')
+
+        # check if one recipe exist
+        self.assertIn(needed_title, content)
+
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
@@ -56,3 +66,18 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(
             reverse('recipes:recipe', kwargs={'id': 1000}))
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_template_loads_the_correct_recipe(self):
+        needed_title = 'This is a detail page -It load one recipe'
+
+        # need a recipe for this test
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(reverse(
+            'recipes:recipe',
+            kwargs={'id': 1}
+        ))
+        content = response.content.decode('utf-8')
+
+        # check if one recipe exist
+        self.assertIn(needed_title, content)
