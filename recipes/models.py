762b331108c6f2c63998515ba7745cc5948d96b4
Werberty <werberty.alexandre@gmail.com>
2022-10-05 09:46:23 -0300

    FIXED: campo slug como unico

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,6 @@
 
+from enum import unique
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -13,7 +15,7 @@ class Category(models.Model):
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
