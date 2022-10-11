4fc318d7837118a641e40882ee6cd1a38a9d084e
Werberty <werberty.alexandre@gmail.com>
2022-10-11 09:07:50 -0300

    is_valid para salvar formulário

--- authors/forms.py
+++ authors/forms.py
@@ -1,4 +1,4 @@
-from distutils.command.clean import clean
+import re
 
 from django import forms
 from django.contrib.auth.models import User
@@ -12,6 +12,19 @@ def add_attr(field, attr_name, attr_new_val):
 
 def add_placeholder(field, placeholder_val):
     field.widget.attrs['placeholder'] = placeholder_val
+
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. the length should be '
+            'at least 8 characters'
+        ),
+            code='invalid'
+        )
 
 
 class RegisterForm(forms.ModelForm):
@@ -32,10 +45,11 @@ class RegisterForm(forms.ModelForm):
             'required': 'Password must not be empty'
         },
         help_text=(
-            'Passwor must have at least one uppercase letter, '
+            'Password must have at least one uppercase letter, '
             'one lowercase letter and one number. the length should be '
             'at least 8 characters'
-        )
+        ),
+        validators=[strong_password]
     )
     password2 = forms.CharField(
         required=True,
