187b4ef25697a1e72fe87b7745a1f45daa15d5b1
Werberty <werberty.alexandre@gmail.com>
2022-10-18 17:47:24 -0300

    Organizando os teste e criando Mixins

--- tests/functional_tests/recipes/base.py
+++ tests/functional_tests/recipes/base.py
@@ -1,11 +1,12 @@
 import time
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.common.by import By
 from utils.browser import make_chrome_browser
 
+from recipes.tests.test_recipe_base import RecipeMixing
 
-class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
+
+class RecipeBaseFunctionalTest(StaticLiveServerTestCase, RecipeMixing):
     def setUp(self) -> None:
         self.browser = make_chrome_browser('--headlees')
         return super().setUp()
@@ -16,10 +17,3 @@ class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
 
     def sleep(self, seconds=5):
         time.sleep(seconds)
-
-
-class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
-    def test_recipe_home_page_without_recipes_not_found_message(self):
-        self.browser.get(self.live_server_url)
-        body = self.browser.find_element(By.TAG_NAME, 'body')
-        self.assertIn('No recipes found here', body.text)
