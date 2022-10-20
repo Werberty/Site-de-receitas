0eca5066038a3e229cd77ec91e2d4335f9bfa1e7
Werberty <werberty.alexandre@gmail.com>
2022-10-19 21:45:11 -0300

    FEAT: Formulário de edição de receita

--- authors/views.py
+++ authors/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from recipes.models import Recipe
+
+from authors.forms.recipe_form import AuthorRecipeForm
 
 from .forms import LoginForm, RegisterForm
 
@@ -87,4 +90,32 @@ def logout_view(request):
 
 @login_required(login_url='authors:login', redirect_field_name='next')
 def dashboard(request):
-    return render(request, 'authors/pages/dashboard.html')
+    recipes = Recipe.objects.filter(
+        is_published=False,
+        author=request.user
+    )
+    return render(request, 'authors/pages/dashboard.html', context={
+        'recipes': recipes,
+    })
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_edit(request, id):
+    recipe = Recipe.objects.get(
+        is_published=False,
+        author=request.user,
+        pk=id,
+    )
+
+    if not recipe:
+        raise Http404()
+
+    form = AuthorRecipeForm(
+        request.POST or None,
+        instance=recipe
+    )
+
+    return render(request, 'authors/pages/dashboard_recipe.html', context={
+        'recipe': recipe,
+        'form': form,
+    })
